12863ed5e4216f99d577dd5a49a74cd49750d073
Ethan Nguyen <nguyenethan01@gmail.com>
2020-07-09 22:50:47 +0000

    Copy Usable Code

--- website/src/app/views/about/About.js
+++ website/src/app/views/about/About.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
+
+
 
 function About() {
   return (
+    <div>
       <h1>About Page</h1>
+      <ProfileCard major = "CS @ Stanford" bio = "enjoys being a very good boy" name = "Doggy Dog" imageURL = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg" instagramURL = "https://www.instagram.com/eethannugget/"> </ProfileCard>
+    </div>
   );
 }
 
