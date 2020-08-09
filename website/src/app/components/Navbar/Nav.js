0f9d87af549e3479bcfe7222caa3685c3c5dd1df
Ethan Nguyen <nguyenethan01@gmail.com>
2020-08-09 23:25:09 +0000

    en-fix-lint

--- website/src/app/components/Navbar/Nav.js
+++ website/src/app/components/Navbar/Nav.js
@@ -4,14 +4,12 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <nav>
-        <Link to="/">CtC Home</Link>
-        <Link to="/about" className="link">About</Link>
-        <Link to="/projects" className="link">Projects</Link>
-        <Link to="/contact" className="link">Contact Us</Link>
-        
+      <Link to="/">CtC Home</Link>
+      <Link to="/about" className="link">About</Link>
+      <Link to="/projects" className="link">Projects</Link>
+      <Link to="/contact" className="link">Contact Us</Link>
     </nav>
   );
 }
 
 export default Nav;
-
