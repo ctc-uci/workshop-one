12a0b865695bf6372a031154ac10cdcdf24dc6a4
Mingjia Wang <mingjiawangdev@gmail.com>
2020-08-13 21:34:35 -0700

    Change layout from grid to flexbox

--- website/src/app/views/home/Home.js
+++ website/src/app/views/home/Home.js
@@ -7,23 +7,20 @@ function Home() {
       <main>
         <img src="https://illlustrations.co/static/74898b728451a18443001cffcfaf7834/119-working.png" alt="" />
         <div className="right">
-          <h1>Commit the Change</h1>
-          <p>
-            We are a student organization at UC Irvine with a passion for
-            technology and community service. Our team of student designers
-            and engineers work to create sleek, modern websites for nonprofit
-            organizations, completely free of charge!
-          </p>
-          <button id="learn-more" type="button">Learn More</button>
+          <div className="text">
+            <h1>Commit the Change</h1>
+            <p>
+              We are a student organization at UC Irvine with a passion for
+              technology and community service. Our team of student designers
+              and engineers work to create sleek, modern websites for nonprofit
+              organizations, completely free of charge!
+            </p>
+            <button id="learn-more" type="button">Learn More</button>
+          </div>
         </div>
         <div className="bottom-panel">
-          <h2>Connect with Us</h2>
-          <p>Join us on our mission to create Tech with Purpose</p>
-          <div className="logoborder">
-            <a href="https://facebook.com" aria-label="Facebook URL"><i className="fa fa-facebook" /></a>
-            <a href="https://instagram.com" aria-label="Instagram URL"><i className="fa fa-instagram" /></a>
-            <a href="https://linkedin.com" aria-label="Linkedin URL"><i className="fa fa-linkedin" /></a>
-          </div>
+          <h2>Students</h2>
+          <p>Want to join the team?</p>
         </div>
       </main>
     </div>
