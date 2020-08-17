9d7f33ab0aa23e17b221f57aa205416d2d3270a8
Mingjia Wang <mingjiawangdev@gmail.com>
2020-08-16 17:13:51 -0700

    Fix flexbox configuration and global font settings

--- website/src/app/views/home/Home.js
+++ website/src/app/views/home/Home.js
@@ -4,21 +4,23 @@ import './Home.css';
 function Home() {
   return (
     <main>
-      <div className="top-left">
-        <div className="image">
-          <img src="https://illlustrations.co/static/74898b728451a18443001cffcfaf7834/119-working.png" alt="Person" />
+      <div className="top-panel">
+        <div className="top-left">
+          <div className="image">
+            <img src="https://illlustrations.co/static/74898b728451a18443001cffcfaf7834/119-working.png" alt="Person" />
+          </div>
         </div>
-      </div>
-      <div className="top-right">
-        <div className="text">
-          <h1>Commit the Change</h1>
-          <p>
-            We are a student organization at UC Irvine with a passion for
-            technology and community service. Our team of student designers
-            and engineers work to create sleek, modern websites for nonprofit
-            organizations, completely free of charge!
-          </p>
-          <button id="learn-more" type="button">Learn More</button>
+        <div className="top-right">
+          <div className="text">
+            <h1>Commit the Change</h1>
+            <p>
+              We are a student organization at UC Irvine with a passion for
+              technology and community service. Our team of student designers
+              and engineers work to create sleek, modern websites for nonprofit
+              organizations, completely free of charge!
+            </p>
+            <button id="learn-more" type="button">Learn More</button>
+          </div>
         </div>
       </div>
       <div className="bottom-panel">
