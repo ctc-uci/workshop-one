5f89504f208c922e37c9794f412d127e816c11ce
Kayla Tran <kaylat1@uci.edu>
2020-09-20 05:26:36 +0000

    [FEAT] Update about page text and styling

--- website/src/app/views/about/About.js
+++ website/src/app/views/about/About.js
@@ -59,19 +59,19 @@ function About() {
       <div className="links">
         <a href="#mission">
           <h2>
-            Mission &#62;
+            Our Mission &#62;
             <span className="off">&#62;</span>
           </h2>
         </a>
         <a href="#values">
           <h2>
-            Values &#62;
+            Our Values &#62;
             <span className="off">&#62;</span>
           </h2>
         </a>
         <a href="#team">
           <h2>
-            Team &#62;
+            The Team &#62;
             <span className="off">&#62;</span>
           </h2>
         </a>
@@ -80,19 +80,16 @@ function About() {
         <div className="mission-text">
           <h1>Our Mission</h1>
           <p>
-            <span className="chevron">&#8250;</span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt
+            <span className="chevron">&#8250; </span>
+            Deliver high-quality software for nonprofit organizations free of charge
           </p>
           <p>
-            <span className="chevron">&#8250;</span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt
+            <span className="chevron">&#8250; </span>
+            Promote the use of technology for social good
           </p>
           <p>
-            <span className="chevron">&#8250;</span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt
+            <span className="chevron">&#8250; </span>
+            Uphold our five core values in everything we do
           </p>
         </div>
         <div className="ctc-logo">
@@ -102,8 +99,9 @@ function About() {
       <div className="our-values-panel" id="values">
         <h1>Our Values &#38; Culture</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Commit the Change understands the importance of culture and is committed to standing for these common values.
+          These are not just hollow sayings, we will hold ourselves accountable to constantly improving and
+          challenging ourselves through our actions and words.
         </p>
         <div className="value-row-1">
           <div className="value">
@@ -112,8 +110,8 @@ function About() {
             </span>
             <h2>Develop Community</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Our goal is to bring the whole organization together as one strong, helpful, and supportive community as friends
+              and teammates.
             </p>
           </div>
           <div className="value">
@@ -122,8 +120,8 @@ function About() {
             </span>
             <h2>Build Enduring Products</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              We aim to build products that are maintainable and sustainable for long-term use. This involves high quality
+              development, design, and documentation.
             </p>
           </div>
           <div className="value">
@@ -132,8 +130,8 @@ function About() {
             </span>
             <h2>Be Authentic</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              We want every member to feel safe being honest and true to their authentic self.
+              Respect other members by being open to other opinions and perspectives.
             </p>
           </div>
         </div>
@@ -144,8 +142,8 @@ function About() {
             </span>
             <h2>Take Initiative</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              We want every member to feel comfortable trying and learning new things.
+              Failure is accepted and encouraged, as long as you continue to try again.
             </p>
           </div>
           <div className="value">
@@ -154,8 +152,8 @@ function About() {
             </span>
             <h2>Strive for Social Impact</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              We aim to come up with creative solutions when designing our products to make a difference in the community
+              and strive to pick partnerships with organizations that need our help the most, thus making the largest impact.
             </p>
           </div>
         </div>
@@ -168,7 +166,7 @@ function About() {
         >
           <h1>Our Team</h1>
         </VisibilitySensor>
-
+        <p className="our-team-description">Meet our team of hard-working designers and developers!</p>
         <div className="team-photos">{profileImages}</div>
       </div>
     </main>
