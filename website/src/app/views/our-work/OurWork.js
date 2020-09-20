c828126f47911dc52031a7b43921d886dd18e01d
Albert Zhang <albert@alberts-air.lan>
2020-09-19 23:43:48 -0400

    scrolling animations for our work page

--- website/src/app/views/our-work/OurWork.js
+++ website/src/app/views/our-work/OurWork.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './OurWork.css';
-import { animated, useSpring } from 'react-spring';
+import { animated, useSpring, useTrail } from 'react-spring';
+import VisibilitySensor from 'react-visibility-sensor';
 import animationConfig from '../animationConstants';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import ProjectCardPurple from '../../components/ProjectCardPurple/ProjectCardPurple';
 import ourWorkPic from '../../../images/ourWork/our-work.svg';
-import * as projectData from './ProjectCard.json';
-
-// NPO Logos
-import childCreativityLab from '../../../images/nonprofits/Child-Creativity-Lab.png';
-import justiceForFamilies from '../../../images/nonprofits/Justice-For-Families.png';
-import waymakers from '../../../images/nonprofits/Waymakers.png';
-import crimeSurvivors from '../../../images/nonprofits/our-work-crime-survivors.svg';
+import projects from './projectsData';
 
 function OurWork() {
-  const slideUp = useSpring(animationConfig.slideUp(true));
+  const [middleViewCount, setMiddleVisible] = useState(0);
+  const [bottomViewCount, setBottomVisible] = useState(0);
+
+  const slideUpTop = useSpring(animationConfig.slideUp(true));
+  const slideUpMiddle = useSpring(animationConfig.slideUp(middleViewCount > 0));
+  const activeProjectsTrail = useTrail(projects.active.length, animationConfig.trail(middleViewCount > 0));
+  const slideUpInactiveProjects = useSpring(animationConfig.slideUp(bottomViewCount > 0, 500));
+  const slideInBottomText = useSpring(animationConfig.slideInLeft(bottomViewCount > 0));
+
+  const activeProjectsAnimation = activeProjectsTrail.map((props, index) => {
+    const project = projects.active[index];
+    return (
+      <ProjectCard
+        imageURL={project.imageURL}
+        projectName={project.projectName}
+        description={project.description}
+        projectURL={project.projectURL}
+        animationProps={props}
+        isAnimated
+      />
+    );
+  });
 
   return (
     <main>
-      <animated.div style={slideUp} className="top-panel">
+      <animated.div style={slideUpTop} className="top-panel">
         <div className="top-panel-text">
           <div className="inside-top-panel-text">
             <h1 className="top-panel-title">Our Work</h1>
@@ -38,66 +54,48 @@ function OurWork() {
         </div>
       </animated.div>
       <div id="middle-our-work">
-        <div className="project-text">
-          <h1 className="upcoming-projects">Our Upcoming Projects</h1>
-          <p className="upcoming-projects-description">
-            We are proud to present the below projects for the 2020-2021
-            academic year!
-          </p>
-        </div>
-        <div className="projects">
-          <ProjectCard
-            className="projectcard"
-            id="ccl"
-            imageURL={childCreativityLab}
-            projectName={projectData.project1.projectName}
-            description={projectData.project1.description}
-            projectURL={projectData.project1.projectURL}
-          />
-          <ProjectCard
-            className="projectcard"
-            imageURL={justiceForFamilies}
-            projectName={projectData.project2.projectName}
-            description={projectData.project2.description}
-            projectURL={projectData.project2.projectURL}
-          />
-          <ProjectCard
-            className="projectcard"
-            imageURL={waymakers}
-            projectName={projectData.project3.projectName}
-            description={projectData.project3.description}
-            projectURL={projectData.project3.projectURL}
-          />
-        </div>
+        <VisibilitySensor onChange={(isVisible) => { if (isVisible) setMiddleVisible(middleViewCount + 1); }}>
+          <animated.div style={slideUpMiddle} className="project-text">
+            <h1 className="upcoming-projects">Our Upcoming Projects</h1>
+            <p className="upcoming-projects-description">
+              We are proud to present the below projects for the 2020-2021
+              academic year!
+            </p>
+          </animated.div>
+        </VisibilitySensor>
+        <div className="projects">{activeProjectsAnimation}</div>
       </div>
       <div className="past-project-section">
         <div className="past-project-div">
-          <div className="past-project-content">
-            <h1 className="past-project-title">Our Past Project</h1>
-            <p className="past-project-description">
-              Commit the Change started its first project in April 2020 with the
-              nonprofit Crime Survivors. This organization wanted a way to put their paper pamplets online, so that the
-              information can be more accessible and to decrease the printing cost of the pamplets.
-            </p>
-            <a
-              href="https://www.google.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="common-pink-button"
-            >
-              View Project
-            </a>
-          </div>
+          <VisibilitySensor onChange={(isVisible) => { if (isVisible) setBottomVisible(bottomViewCount + 1); }}>
+            <animated.div style={slideInBottomText} className="past-project-content">
+              <h1 className="past-project-title">Our Past Project</h1>
+              <p className="past-project-description">
+                Commit the Change started its first project in April 2020 with the
+                nonprofit Crime Survivors. This organization wanted a way to put
+                their paper pamplets online, so that the information can be more
+                accessible and to decrease the printing cost of the pamplets.
+              </p>
+              <a
+                href="https://www.google.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="common-pink-button"
+              >
+                View Project
+              </a>
+            </animated.div>
+          </VisibilitySensor>
         </div>
         <div className="bottom-card-div">
-          <div className="bottom-card">
+          <animated.div style={slideUpInactiveProjects} className="bottom-card">
             <ProjectCardPurple
-              projectName={projectData.project4.projectName}
-              imageURL={crimeSurvivors}
-              description={projectData.project4.description}
-              projectURL={projectData.project4.projectURL}
+              projectName={projects.inactive[0].projectName}
+              imageURL={projects.inactive[0].imageURL}
+              description={projects.inactive[0].description}
+              projectURL={projects.inactive[0].projectURL}
             />
-          </div>
+          </animated.div>
         </div>
       </div>
     </main>
