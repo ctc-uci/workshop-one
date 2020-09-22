ffc6362018c54f43920b5c6a1857631f5e99aeaf
Albert Zhang <aalbertzhang@gmail.com>
2020-09-22 05:23:10 +0000

    bring back mission bar and animate FAQ

--- website/src/app/components/Question/Question.js
+++ website/src/app/components/Question/Question.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Question.css';
+import {
+  animated, useSpring, useTransition,
+} from 'react-spring';
+import animationConfigs from '../../views/animationConstants';
 
-function Question(props) {
+function Question(styleProps) {
   const {
     index, questionText, answerText, expanded, handleClick,
-  } = props;
+  } = styleProps;
+
+  const slideDown = useSpring(animationConfigs.openQuestion(expanded));
+
+  const transitions = useTransition(expanded, null, {
+    from: { position: 'absolute', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  const x = transitions.map(({ item, props }) => (item
+    ? <animated.div style={props}>+</animated.div>
+    : <animated.div className="minus" style={props}>-</animated.div>));
 
   return (
-    <div className="question-card" onClick={() => handleClick(index)} onKeyDown={() => handleClick(index)} role="tab" tabIndex={0}>
+    <div onClick={() => { handleClick(index); }} onKeyDown={() => { handleClick(index); }} className="question-card" role="tab" tabIndex={0}>
       <div className="top-half">
         <div className="symbol">
-          <span className="expand-symbol">{expanded ? '-' : '+'}</span>
+          <span className="expand-symbol">{x}</span>
         </div>
-        <h2 className="question-text">{questionText}</h2>
+        <h2 style={expanded ? { color: '#ed315d' } : null} className="question-text">{questionText}</h2>
       </div>
-      <div className={(expanded ? 'bottom-half' : 'hidden')}>
+      <animated.div style={slideDown} className="bottom-half">
         <p>{answerText}</p>
-      </div>
+      </animated.div>
     </div>
   );
 }
-
-Question.propTypes = {
-  index: PropTypes.number.isRequired,
-  questionText: PropTypes.string.isRequired,
-  answerText: PropTypes.string.isRequired,
-  expanded: PropTypes.bool.isRequired,
-  handleClick: PropTypes.func.isRequired,
-};
 
 export default Question;
