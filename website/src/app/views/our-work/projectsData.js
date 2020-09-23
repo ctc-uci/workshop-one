4dc99f80dad71e7dcd19178595285d49d0f8d6ce
Kayla Tran <kaylat1@uci.edu>
2020-09-23 22:29:37 +0000

    Revert "Merge branch 'az-faq-animation' into 'dev'"
    This reverts merge request !74

--- website/src/app/views/our-work/projectsData.js
+++ website/src/app/views/our-work/projectsData.js
@@ -11,6 +11,7 @@ const projects = {
       description:
         'We will create an online waiver system and a platform to sell educational kits for children.',
       projectURL: 'https://medium.com/@committhechange.uci/upcoming-project-1-reveal-with-child-creativity-lab-e9fb41fe7835',
+      npoURL: 'https://www.childcreativitylab.org/',
     },
     {
       imageURL: jff,
@@ -18,6 +19,7 @@ const projects = {
       description:
         'We will create a mobile application for the families of incarcerated loved ones to find community and support.',
       projectURL: 'https://medium.com/@committhechange.uci/upcoming-project-2-reveal-with-justice-for-families-57f91752635d',
+      npoURL: 'https://www.justice4families.org/',
     },
     {
       imageURL: wm,
@@ -25,6 +27,7 @@ const projects = {
       description:
         'We will create a platform to electronically capture volunteer and inventory data about their different programs.',
       projectURL: 'https://medium.com/@committhechange.uci/upcoming-project-3-reveal-with-waymakers-4be6f2915c83',
+      npoURL: 'https://waymakersoc.org/',
     },
   ],
   inactive: [
@@ -34,6 +37,7 @@ const projects = {
       description:
         'We created a website to replace their paper pamphlets that detail important resources and information for crime victims in different counties throughout Southern California. This project will save the organization thousands of dollars every year in printing and distribution costs, which will go directly back into helping survivors get necessity items.',
       projectURL: 'https://google.com',
+      npoURL: 'https://crimesurvivors.org/',
     },
   ],
 };
