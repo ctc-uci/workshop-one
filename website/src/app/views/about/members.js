2f8e5924f8f389a7c7bef775b453582a2844c69d
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2021-10-24 10:51:10 -0700

    rw member photo (#34)
    * Create PR for #33
    
    * added profile photo & info for Riley Wong
    
    * moved Riley Wong profile to be in alphabetical order
    
    Co-authored-by: rileysw <rileysw@users.noreply.github.com>
    Co-authored-by: Riley Wong <rileysw@uci.edu>

--- website/src/app/views/about/members.js
+++ website/src/app/views/about/members.js
@@ -45,6 +45,7 @@ import vivian from '../../../images/about/memberImages/vivian.jpg';
 import jordan from '../../../images/about/memberImages/jordan.jpg';
 import sam from '../../../images/about/memberImages/sam.jpg';
 import rileychampion from '../../../images/about/memberImages/rileychampion.jpg';
+import rileywong from '../../../images/about/memberImages/rileywong.jpg';
 
 const members = {
   info: [
@@ -254,7 +255,7 @@ const members = {
       imageURL: phillip,
     },
     {
-      name: 'Po\'okela Moana',
+      name: "Po'okela Moana",
       position: 'Developer',
       linkedinURL: 'https://www.linkedin.com/in/cullen-moana/',
       imageURL: kela,
@@ -270,6 +271,12 @@ const members = {
       position: 'Developer',
       linkedinURL: 'https://www.linkedin.com/in/rileychampion/',
       imageURL: rileychampion,
+    },
+    {
+      name: 'Riley Wong',
+      position: 'Developer',
+      linkedinURL: 'https://www.linkedin.com/in/riley-wong/',
+      imageURL: rileywong,
     },
     {
       name: 'Sam Der',
