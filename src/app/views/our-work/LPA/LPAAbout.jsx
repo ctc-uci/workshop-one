0cb9e184216c500bd2db201f57e5c3cedb153ded
Cole Thompson <colet0227@gmail.com>
2024-11-03 14:55:44 -0800

    feat: implement about page

--- src/app/views/our-work/LPA/LPAAbout.jsx
+++ src/app/views/our-work/LPA/LPAAbout.jsx
@@ -5,38 +5,51 @@ import React from "react";
 import "./LPAAbout.css";
 import Footer from "../../../components/Footer/Footer";
 import LPAImage from '../../../../images/nonprofits/La-Pena.jpg';
+import CTCImage from '../../../../images/logo/purple-ctc.svg';
+import Triangle1 from '../../../../images/shapes/triangle1.png';
+import Triangle2 from '../../../../images/shapes/triangle2.png';
 
 // you can safely ignore most of the things above the opening div tag for now,
 // we'll cover them in more detail in our Frontend/React unit
 const LPAAbout = () => (
   <div className="about-us-page-container">
     {/* your HTML starts here! */}
-
     <main className="about-us-main">
       <div>
-        <section>
+        <div className="header-container">
+          <img src={LPAImage} alt="Stand Up to Trash Logo" style={{ width: '234px', height: '94px' }} />
           <h1>La Peña</h1>
-
-          <img src={LPAImage} alt="Stand Up to Trash Logo" style={{ width: '300px', height: 'auto' }} />
-
-          {/* for your convenience, some styles defined elsewhere */}
-          <button type="button" className="primary-button">Primary Button</button>
-          <button type="button" className="secondary-button">Secondary Button</button>
-        </section>
+          <button type="button" className="primary-button">GitHub</button>
+          <button type="button" className="secondary-button">Official Site</button>
+        </div>
         <div className="about-us-content">
+          <p class="timeline-text">Fall 2024 - Spring 2025</p>
           <section>
             <h2>Mission</h2>
+            <p>Since 1975, La Peña Cultural Center has been a central site for Latinx, Caribbean, and Indigenous diasporic communities of the Bay Area to preserve and celebrate cultural traditions, present new interdisciplinary creative works, and to nurture grassroots social justice movements with artists, activists, and allies.</p>
           </section>
           <section>
             <h2>Problem</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </section>
           <section>
-            <h2>CTC&#39;s Solution</h2>
+            
+            
+            <div class="header-text">
+              <img src={CTCImage} alt="CTC Logo" class="header-logo"></img>
+              <h2>CTC&#39;s Solution</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            
           </section>
         </div>
       </div>
-
     </main>
+    
+    <div class="graphics-container">
+      <img src={Triangle1} alt="Triangle 1" class="triangle1"></img>
+      <img src={Triangle2} alt="Triangle 2" class="triangle2"></img>
+    </div>
 
     {/* your HTML ends here! */}
 
